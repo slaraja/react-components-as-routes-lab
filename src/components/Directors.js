43e15d2f25c8933e7338b83e08c5a0338c25f18f
Stephanie La Raja <stephanielaraja@gmail.com>
2021-10-03 09:48:36 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { directors } from '../data';
 
+
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+return (
+  <div>
+    <h1>Directors Page</h1>
+    {directors.map((director, index) => (
+      <div key={index}>
+        <h3>Name: {director.name}</h3>
+        <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie, index) =>(
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+      </div>
+    ))}
+  </div>
+);
 }
+
+
+// let directors = [
+//   {
+//     name: 'Scott Derrickson',
+//     movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose']
+//   },
 
 export default Directors
